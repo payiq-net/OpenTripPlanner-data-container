1e6c41da6c33ec6b8371bf29107ade232bfbb3d7
Joel Lappalainen <lappalj8@gmail.com>
2021-09-06 11:05:17 +0300

    Install and use python3 libraries

--- otp-data-tools/build-otp-data-tools.sh
+++ otp-data-tools/build-otp-data-tools.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 # this script is run inside the otp-data-tools container
-set +e
+set -e
 apt-get update && \
   apt-get -y install \
     git build-essential python-dev protobuf-compiler libprotobuf-dev \
     make swig g++ python-dev libreadosm-dev \
     libboost-graph-dev libproj-dev libgoogle-perftools-dev \
-    osmctools unzip zip python-pyproj wget python-argh \
-    python-scipy python-sklearn python-pip python-numpy curl
+    osmctools unzip zip wget python3-pyproj python3-argh \
+    python3-scipy python3-sklearn python3-pip python3-numpy curl
 
 rm -rf /var/lib/apt/lists/*
 
-wget https://bootstrap.pypa.io/pip/2.7/get-pip.py && \
-  python get-pip.py && \
-  pip install imposm.parser && \
-  pip install argh && \
-  pip install future && \
-  pip install grequests && \
-  pip install unicodecsv && \
-  pip install cffi && \
-  pip install utm
+pip3 install imposm.parser && \
+  pip3 install argh && \
+  pip3 install future && \
+  pip3 install grequests && \
+  pip3 install unicodecsv && \
+  pip3 install cffi && \
+  pip3 install utm
 
 mkdir -p one-busaway-gtfs-transformer && \
   wget -O one-busaway-gtfs-transformer/onebusaway-gtfs-transformer-cli.jar "http://nexus.onebusaway.org/service/local/artifact/maven/content?r=public&g=org.onebusaway&a=onebusaway-gtfs-transformer-cli&v=1.3.9"
@@ -27,8 +25,8 @@ mkdir -p one-busaway-gtfs-transformer && \
 git clone https://github.com/jswhit/pyproj.git
 cd pyproj
 git checkout ec9151e8c6909f7fac72bb2eab927ff18fa4cf1d
-python setup.py build
-python setup.py install
+python3 setup.py build
+python3 setup.py install
 cd ..
 
 git clone --recursive -b fastmapmatch https://github.com/HSLdevcom/gtfs_shape_mapfit.git
