bb50750e345014955cbaf6e0695bd428f55c87ea
Vesa Meskanen <vesa.meskanen@cgi.com>
2022-06-15 14:05:53 +0300

    Pass ROUTER_NAME env to OTP

--- task/buildOTPGraph.js
+++ task/buildOTPGraph.js
@@ -21,7 +21,7 @@ const buildGraph = function (config) {
     const version = execSync(`docker pull hsldevcom/opentripplanner:${graphBuildTag};docker run --rm --entrypoint /bin/bash hsldevcom/opentripplanner:${graphBuildTag}  -c "java -jar otp-shaded.jar --version"`)
     const commit = version.toString().match(/commit: ([0-9a-f]+)/)[1]
 
-    const buildGraph = exec(`docker run -v ${hostDataDir}/build:/opt/opentripplanner/graphs --mount type=bind,source=/opt/otp-data-builder/logback-include-extensions.xml,target=/opt/opentripplanner/logback-include-extensions.xml --rm --entrypoint /bin/bash hsldevcom/opentripplanner:${graphBuildTag}  -c "java -Xmx9g -jar otp-shaded.jar --build --save ./graphs/${config.id}/router"`, { maxBuffer: constants.BUFFER_SIZE })
+    const buildGraph = exec(`docker run -v ${hostDataDir}/build:/opt/opentripplanner/graphs --mount type=bind,source=/opt/otp-data-builder/logback-include-extensions.xml,target=/opt/opentripplanner/logback-include-extensions.xml -e ROUTER_NAME=${process.env.ROUTER_NAME} --rm --entrypoint /bin/bash hsldevcom/opentripplanner:${graphBuildTag}  -c "java -Xmx9g -jar otp-shaded.jar --build --save ./graphs/${config.id}/router"`, { maxBuffer: constants.BUFFER_SIZE })
     // const buildGraph = exec('ls -la');
     const buildLog = fs.openSync(`${dataDir}/build/${config.id}/build.log`, 'w+')
 
