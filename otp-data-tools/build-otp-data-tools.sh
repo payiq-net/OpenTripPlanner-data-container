118f0db9e033272b4f47b2b0dffa83d1cd73ff09
Joona Ojapalo <joona.ojapalo@cgi.fi>
2022-08-24 12:26:32 +0300

    Halt otp-data-tools image build process if error in any step & report to slack

--- otp-data-tools/build-otp-data-tools.sh
+++ otp-data-tools/build-otp-data-tools.sh
@@ -1,16 +1,24 @@
-#!/bin/bash
-# this script is run inside the otp-data-tools container
+#!/usr/bin/env bash
+
+# This script is run inside the otp-data-tools container.
+
+# import die_with_error
+source ./reporting-functions.sh
+
 set +e
+
+# shellcheck disable=2015
 apt-get update && \
   apt-get -y install \
     git build-essential python-dev protobuf-compiler libprotobuf-dev \
     make swig g++ python-dev libreadosm-dev \
     libboost-graph-dev libproj-dev libgoogle-perftools-dev \
     osmctools unzip zip python-pyproj wget python-argh \
-    python-scipy python-sklearn python-pip python-numpy curl
+    python-scipy python-sklearn python-pip python-numpy curl || die_with_error "apt-get update failed"
 
 rm -rf /var/lib/apt/lists/*
 
+# shellcheck disable=2015
 wget https://bootstrap.pypa.io/pip/2.7/get-pip.py && \
   python get-pip.py && \
   pip install imposm.parser && \
@@ -19,24 +27,30 @@ wget https://bootstrap.pypa.io/pip/2.7/get-pip.py && \
   pip install grequests && \
   pip install unicodecsv && \
   pip install cffi && \
-  pip install utm
+  pip install utm || die_with_error "installation of python packages failed"
 
+ONEBUSAWAY_URL="http://nexus.onebusaway.org/service/local/artifact/maven/content?r=public&g=org.onebusaway&a=onebusaway-gtfs-transformer-cli&v=1.3.9"
+
+# shellcheck disable=2015
 mkdir -p one-busaway-gtfs-transformer && \
-  wget -O one-busaway-gtfs-transformer/onebusaway-gtfs-transformer-cli.jar "http://nexus.onebusaway.org/service/local/artifact/maven/content?r=public&g=org.onebusaway&a=onebusaway-gtfs-transformer-cli&v=1.3.9"
+  wget -O one-busaway-gtfs-transformer/onebusaway-gtfs-transformer-cli.jar $ONEBUSAWAY_URL || die_with_error "onebusaway-gtfs-transformer-cli fetch failed"
 
-git clone https://github.com/jswhit/pyproj.git
-cd pyproj
-git checkout ec9151e8c6909f7fac72bb2eab927ff18fa4cf1d
-python setup.py build
-python setup.py install
+# shellcheck disable=2015
+git clone https://github.com/jswhit/pyproj.git && \
+  cd pyproj && \
+  git checkout ec9151e8c6909f7fac72bb2eab927ff18fa4cf1d && \
+  python setup.py build && \
+  python setup.py install || die_with_error "pyproj build failed"
 cd ..
 
-git clone --recursive -b fastmapmatch https://github.com/HSLdevcom/gtfs_shape_mapfit.git
-cd gtfs_shape_mapfit
-make -C pymapmatch
+# shellcheck disable=2015
+git clone --recursive -b fastmapmatch https://github.com/HSLdevcom/gtfs_shape_mapfit.git && \
+  cd gtfs_shape_mapfit && \
+  make -C pymapmatch || die_with_error "installation of gtfs_shape_mapfit failed"
 cd ..
 
-git clone https://github.com/HSLdevcom/OTPQA.git
-cd OTPQA
-git checkout v3
+# shellcheck disable=2015
+git clone https://github.com/HSLdevcom/OTPQA.git && \
+  cd OTPQA && \
+  git checkout v3 || die_with_error "installation of OTPQA update failed"
 cd ..
