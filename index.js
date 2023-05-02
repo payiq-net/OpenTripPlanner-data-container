079061dd49e1b18125bca2a6879adaf6099ca5d4
Vesa Meskanen <vesa.meskanen@cgi.com>
2023-05-02 10:58:12 +0300

    Try skipping seed stage

--- index.js
+++ index.js
@@ -26,20 +26,22 @@ if (process.env.ROUTERS) {
   routers = ['finland', 'waltti', 'hsl', 'waltti-alt', 'varely', 'kela']
 }
 
-start('seed').then(() => {
-  process.stdout.write('Seeded.\n')
-  if (process.argv.length === 3 && process.argv[2] === 'once') {
-    process.stdout.write('Running update once.\n')
-    update()
-  } else {
-    const cronPattern = process.env.CRON || '0 0 3 * * *'
-    process.stdout.write(`Starting timer with pattern: ${cronPattern}\n`)
-    new CronJob(cronPattern, update, null, true, 'Europe/Helsinki') // eslint-disable-line
-  }
-}).catch((err) => {
-  process.stdout.write(err + '\n')
-  process.exit(1)
-})
+if (!process.env.NOSEED) {
+  start('seed').then(() => {
+    process.stdout.write('Seeded.\n')
+    if (process.argv.length === 3 && process.argv[2] === 'once') {
+      process.stdout.write('Running update once.\n')
+      update()
+    } else {
+      const cronPattern = process.env.CRON || '0 0 3 * * *'
+      process.stdout.write(`Starting timer with pattern: ${cronPattern}\n`)
+      new CronJob(cronPattern, update, null, true, 'Europe/Helsinki') // eslint-disable-line
+    }
+  }).catch((err) => {
+    process.stdout.write(err + '\n')
+    process.exit(1)
+  })
+}
 
 async function update () {
   const slackResponse = await postSlackMessage('Starting data build')
