3f6d148d86b9c2d13e42378b06a32013dee4406e
Vesa Meskanen <vesa.meskanen@cgi.com>
2023-05-09 10:40:22 +0300

    Move opt-matching from separate repo to util, remove unused failing query

--- util.js
+++ util.js
@@ -1,6 +1,8 @@
 const JSZip = require('jszip')
 const fs = require('fs')
 const globby = require('globby')
+const readline = require("readline");
+const path = require("path");
 
 const request = require('request')
 const { promisify } = require('util')
@@ -108,6 +110,66 @@ const compareSizes = (localFile, newFileSize, maxDifference) => {
   })
 }
 
+
+const UNCONNECTED = /Could not connect ([A-Z]?[a-z]?\d{4}) at \((\d+\.\d+), (\d+\.\d+)/;
+const CONNECTED = /Connected <.*:(\d*) lat,lng=(\d+\.\d+),(\d+\.\d+)> \(([A-Z]?[a-z]?\d{4})\) to (.*) at \((\d+\.\d+), (\d+\.\d+)/;
+
+function distance(lat1, lon1, lat2, lon2) {
+  var p = Math.PI / 180;
+  var a =
+    0.5 -
+    Math.cos((lat2 - lat1) * p) / 2 +
+    Math.cos(lat1 * p) * Math.cos(lat2 * p) * (1 - Math.cos((lon2 - lon1) * p)) / 2;
+
+  return 12742 * 1000 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+}
+
+async function match(line, connectedStream, unconnectedStream) {
+  let res = UNCONNECTED.exec(line);
+  if (res != null) {
+    const [stop_code, jore_lon, jore_lat] = res.slice(1);
+    let departures;
+    unconnectedStream.write([stop_code, jore_lat, jore_lon].join(",") + "\n");
+    return;
+  }
+  res = CONNECTED.exec(line);
+  if (res != null) {
+    const [stop_id, jore_lat, jore_lon, stop_code, osm_node, osm_lon, osm_lat] = res.slice(1);
+    const dist = distance(jore_lat, jore_lon, osm_lat, osm_lon);
+    connectedStream.write(
+      [stop_id, stop_code, jore_lat, jore_lon, osm_node, osm_lat, osm_lon, dist].join(",") + "\n",
+    );
+    return;
+  }
+}
+
+// process taggedStops.log file into connected.csv and unconnected.csv in given dir path
+const otpMatching = function (directory) {
+  return new Promise(resolve => {
+    const promises = [];
+
+    const connectedStream = fs.createWriteStream(path.join(directory, "connected.csv"));
+    const unconnectedStream = fs.createWriteStream(path.join(directory, "unconnected.csv"));
+    connectedStream.write(
+      "stop_id,stop_code,jore_lat,jore_lon,osm_node,osm_lat,osm_lon,distance\n",
+    );
+    unconnectedStream.write("stop_code,jore_lat,jore_lon\n");
+
+    const rl = readline.createInterface({
+      input: fs.createReadStream(path.join(directory, "taggedStops.log")),
+    });
+
+    rl.on("line", line => {
+      promises.push(match(line, connectedStream, unconnectedStream, router));
+    });
+
+    rl.on("close", () => {
+      Promise.all(promises).then(resolve);
+    });
+  });
+};
+
+
 module.exports = {
   zipDir: (zipFile, dir, cb) => {
     zipWithGlob(zipFile, [`${dir}/*`], undefined, cb)
@@ -116,5 +178,6 @@ module.exports = {
   routerDir: (config) => `router-${config.id}`,
   postSlackMessage,
   updateSlackMessage,
-  compareSizes
+  compareSizes,
+  otpMatching
 }
