c2ec95362b0636dff00e6395dc98046dda1f253f
Vesa Meskanen <vesa.meskanen@cgi.com>
2023-10-24 15:36:19 +0300

    Refactor build graph task

--- task/BuildOTPGraph.js
+++ task/BuildOTPGraph.js
@@ -21,7 +21,7 @@ const buildGraph = function (router) {
     const version = execSync(`docker pull hsldevcom/opentripplanner:${graphBuildTag};docker run --rm --entrypoint /bin/bash hsldevcom/opentripplanner:${graphBuildTag}  -c "java -jar otp-shaded.jar --version"`)
     const commit = version.toString().match(/commit: ([0-9a-f]+)/)[1]
 
-    const buildGraph = exec(`docker run -v ${hostDataDir}/build:/opt/opentripplanner/graphs --mount type=bind,source=/opt/otp-data-builder/logback-include-extensions.xml,target=/opt/opentripplanner/logback-include-extensions.xml -e ROUTER_NAME=${process.env.ROUTER_NAME} --rm --entrypoint /bin/bash hsldevcom/opentripplanner:${graphBuildTag}  -c "java ${JAVA_OPTS} -jar otp-shaded.jar --build --save ./graphs/${router.id}/router"`, { maxBuffer: constants.BUFFER_SIZE })
+    const buildGraph = exec(`docker run -v ${hostDataDir}/build:/opt/opentripplanner/graphs --mount type=bind,source=/Users/vesameskanen/digitransit/otp/logback-include-extensions.xml,target=/opt/opentripplanner/logback-include-extensions.xml -e ROUTER_NAME=${process.env.ROUTER_NAME} --rm --entrypoint /bin/bash hsldevcom/opentripplanner:${graphBuildTag}  -c "java ${JAVA_OPTS} -jar otp-shaded.jar --build --save ./graphs/${router.id}/router"`, { maxBuffer: constants.BUFFER_SIZE })
     // const buildGraph = exec('ls -la');
     const buildLog = fs.openSync(`${dataDir}/build/${router.id}/build.log`, 'w+')
 
@@ -39,7 +39,7 @@ const buildGraph = function (router) {
 
     buildGraph.on('exit', (status) => {
       if (status === 0) {
-        resolve({ commit: commit, router: router })
+        resolve({ commit, router })
       } else {
         const log = lastLog.join('')
         postSlackMessage(`${router.id} build failed: ${status}:${log} :boom:`)
@@ -52,17 +52,17 @@ const buildGraph = function (router) {
 }
 
 const packData = function (commit, router) {
+  const dstPath = `${dataDir}/build/${router.id}`
+  const srcPath = `${dstPath}/router`
+
   const p1 = new Promise((resolve, reject) => {
     process.stdout.write('Creating zip file for router data\n')
-
     const osmFiles = router.osm.map(osm => `${dataDir}/build/${router.id}/router/${osm}.pbf`)
 
     // create zip file for the source data
-    // include all gtfs + osm + router- + build configs
-    zipWithGlob(`${dataDir}/build/${router.id}/router-${router.id}.zip`,
-      [`${dataDir}/build/${router.id}/router/*.zip`, `${dataDir}/build/${router.id}/router/*.json`,
-        ...osmFiles,
-        `${dataDir}/build/${router.id}/router/${router.dem}.tif`],
+    // include all gtfs, osm, dem data and otp configs
+    zipWithGlob(`${dstPath}/router-${router.id}.zip`,
+      [`${srcPath}/*.zip`, `${srcPath}/*.json`, ...osmFiles, `${srcPath}/${router.dem}.tif`],
       `router-${router.id}`,
       (err) => {
         if (err) {
@@ -76,8 +76,8 @@ const packData = function (commit, router) {
     process.stdout.write('Creating zip file for otp graph\n')
     // create zip file for the graph
     // include  graph.obj, router-config.json and otp-config.json
-    zipWithGlob(`${dataDir}/build/${router.id}/graph-${router.id}-${commit}.zip`,
-      [`${dataDir}/build/${router.id}/router/graph.obj`, `${dataDir}/build/${router.id}/router/router-*.json`, `${dataDir}/build/${router.id}/router/otp-config.json`],
+    zipWithGlob(`${dstPath}/graph-${router.id}-${commit}.zip`,
+      [`${srcPath}/graph.obj`, `${srcPath}/router-config.json`, `${srcPath}/otp-config.json`],
       router.id,
       (err) => {
         if (err) {
@@ -88,7 +88,7 @@ const packData = function (commit, router) {
       })
   })
   const p3 = new Promise((resolve, reject) => {
-    fs.writeFile(`${dataDir}/build/${router.id}/version.txt`, new Date().toISOString(), function (err) {
+    fs.writeFile(`${dstPath}/version.txt`, new Date().toISOString(), function (err) {
       if (err) {
         reject(err)
       } else {
@@ -100,11 +100,8 @@ const packData = function (commit, router) {
 }
 
 module.exports = {
-  buildOTPGraphTask: router => {
-    return buildGraph(router)
-      .then(packData)
-      .then(() => otpMatching(`${dataDir}/build/${router.id}/router`))
-      .then(() => {
-        process.stdout.write('Graph build SUCCESS\n')
-      })
-  } }
+  buildOTPGraphTask: router => buildGraph(router)
+    .then((commit, router) => packData(commit, router))
+    .then(() => otpMatching(`${dataDir}/build/${router.id}/router`))
+    .then(() => process.stdout.write('Graph build SUCCESS\n'))
+}
