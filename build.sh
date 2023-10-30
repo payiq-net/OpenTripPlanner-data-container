6a8ee15e733870737edafebb9037cfd944a43d75
Vesa Meskanen <vesa.meskanen@cgi.com>
2023-10-30 07:24:01 +0200

    No need to stop docker processes within DIND
    Docker in docker does not see or interfere with host environment's
    docker stuff

--- build.sh
+++ build.sh
@@ -6,20 +6,8 @@ ORG=${ORG:-hsldevcom}
 ROUTER_NAME=${ROUTER_NAME:-hsl}
 DOCKER_IMAGE=$ORG/opentripplanner-data-container-$ROUTER_NAME:test
 
-echo "Making sure there are no old test containers or image available"
-docker stop otp-data-finland || true
-docker stop otp-finland || true
-docker stop otp-data-waltti || true
-docker stop otp-waltti || true
-docker stop otp-data-hsl || true
-docker stop otp-hsl || true
-docker stop otp-data-waltti-alt || true
-docker stop otp-waltti-alt || true
-docker stop otp-data-varely || true
-docker stop otp-varely || true
-docker stop otp-data-kela || true
-docker stop otp-kela || true
-docker rmi --force $DOCKER_IMAGE || true
+# remove old version (may be necessary in local use)
+docker rmi --force $DOCKER_IMAGE &> /dev/null
 cd data/build/$ROUTER_NAME
 echo "Building data-container image..."
 docker build -t $DOCKER_IMAGE -f Dockerfile.data-container .
