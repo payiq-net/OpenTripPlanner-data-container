0a30e9ca5eda055dd6a4dbeb9e6ce5b6d06f32ab
Vesa Meskanen <vesa.meskanen@cgi.com>
2023-11-02 11:43:46 +0200

    Update csv-json converter, refactor feedid task

--- task/SetFeedId.js
+++ task/SetFeedId.js
@@ -10,39 +10,32 @@ function createFeedInfo (zip, file, csv, cb) {
   zip.file('feed_info.txt', csv)
   zip.generateNodeStream({ streamFiles: true, compression: 'DEFLATE' })
     .pipe(fs.createWriteStream(file))
-    .on('finish', function () {
-      cb(null)
-    })
+    .on('finish', cb)
 }
 
 function setFeedId (file, id, cb) {
   fs.readFile(file, function (err, data) {
-    if (err) cb(err)
+    if (err) {
+      cb(err)
+      return
+    }
     const zip = new JSZip()
-    zip.loadAsync(data).then(function () {
+    zip.loadAsync(data).then(() => {
       const feedInfo = zip.file('feed_info.txt')
       if (feedInfo === null) {
         const csv = `feed_publisher_name,feed_publisher_url,feed_lang,feed_id
 ${id}-fake-name,${id}-fake-url,${id}-fake-lang,${id}\n`
-
         createFeedInfo(zip, file, csv, () => {
           cb('created') // eslint-disable-line
         })
       } else {
-        // callback function for csv2json converter
-        const csv2jsonCallback = function (err, json) {
-          if (err) {
-            cb(err)
-            return
+        feedInfo.async('string').then(async function (data) {
+          // Remove unnecessary control characters that break things
+          let filteredData = data.replace(/\r/g, '')
+          if (filteredData.charAt(filteredData.length - 1) === '\n') {
+            filteredData = filteredData.slice(0, -1)
           }
-
-          // callback function for json2csv converter
-          /* eslint-disable */
-          const json2csvcallback = function (err, csv) {
-            createFeedInfo(zip, file, csv, () => {
-              cb('edited')
-            })
-          }
+          const json = await converter.csv2json(filteredData)
           /* eslint-enable */
           if (json.length > 0) {
             if (process.env.VERSION_CHECK) {
@@ -55,7 +48,7 @@ ${id}-fake-name,${id}-fake-url,${id}-fake-lang,${id}\n`
                 process.stdout.write('GTFS data for ' + id + ' had not been updated within 8 hours.\n')
                 // send warning also to slack between monday and friday
                 const day = now.getDay()
-                if (day > 0 && day < 6) {
+                if (day !== 1) {
                   postSlackMessage('GTFS data for ' + id + ' had not been updated within 8 hours :boom:')
                 }
               }
@@ -63,21 +56,16 @@ ${id}-fake-name,${id}-fake-url,${id}-fake-lang,${id}\n`
             // no id or id is wrong
             if (json[0].feed_id === undefined || json[0].feed_id !== id) {
               json[0].feed_id = id
-              converter.json2csv(json, json2csvcallback)
+              const csv = await converter.json2csv(json)
+              createFeedInfo(zip, file, csv, () => {
+                cb('edited')  // eslint-disable-line
+              })
             } else {
-              // id was already ok
               cb('nop') // eslint-disable-line
             }
+          } else {
+            cb('nop') // eslint-disable-line
           }
-        }
-
-        feedInfo.async('string').then(function (data) {
-          // Remove unnecessary control characters that break things
-          let filteredData = data.replace(/\r/g, '')
-          if (filteredData.charAt(filteredData.length - 1) === '\n') {
-            filteredData = filteredData.slice(0, -1)
-          }
-          converter.csv2json(filteredData, csv2jsonCallback)
         })
       }
     })
@@ -94,7 +82,7 @@ module.exports = {
       const fileName = gtfsFile.split('/').pop()
       const id = fileName.substring(0, fileName.indexOf('-gtfs'))
       process.stdout.write(gtfsFile + ' ' + 'Setting GTFS feed id to ' + id + '\n')
-      setFeedId(gtfsFile, id, (action) => {
+      setFeedId(gtfsFile, id, action => {
         process.stdout.write(gtfsFile + ' ID ' + action + ' SUCCESS\n')
         file.contents = cloneable(fs.createReadStream(gtfsFile))
         callback(null, file)
