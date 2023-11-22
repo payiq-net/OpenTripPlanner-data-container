1d7e6c79f7fd9b53fd35e8d175cfd51417565b26
Vesa Meskanen <vesa.meskanen@cgi.com>
2023-11-22 10:54:15 +0200

    Remove dependencies which are no longer needed, use python 3

--- otp-data-tools/build-otp-data-tools.sh
+++ otp-data-tools/build-otp-data-tools.sh
@@ -1,53 +1,15 @@
 #!/usr/bin/env bash
-
-# This script is run inside the otp-data-tools container.
-
 set -e
 
-# shellcheck disable=2015
-apt-get update && \
-  apt-get -y install \
-    git build-essential python-dev protobuf-compiler libprotobuf-dev \
-    make swig g++ python-dev libreadosm-dev \
-    libboost-graph-dev libproj-dev libgoogle-perftools-dev \
-    osmctools unzip zip python-pyproj wget python-argh \
-    python-scipy python-sklearn python-pip python-numpy curl
+apt-get update && apt-get -y install git python3 python3-pip
 
 rm -rf /var/lib/apt/lists/*
 
-# shellcheck disable=2015
-wget https://bootstrap.pypa.io/pip/2.7/get-pip.py && \
-  python get-pip.py && \
-  pip install imposm.parser && \
-  pip install argh && \
-  pip install future && \
-  pip install grequests && \
-  pip install unicodecsv && \
-  pip install cffi && \
-  pip install utm
+pip install future grequests numpy
 
 ONEBUSAWAY_URL="http://nexus.onebusaway.org/service/local/artifact/maven/content?r=public&g=org.onebusaway&a=onebusaway-gtfs-transformer-cli&v=1.3.9"
 
-# shellcheck disable=2015
 mkdir -p one-busaway-gtfs-transformer && \
   wget -O one-busaway-gtfs-transformer/onebusaway-gtfs-transformer-cli.jar $ONEBUSAWAY_URL
 
-# shellcheck disable=2015
-git clone https://github.com/jswhit/pyproj.git && \
-  cd pyproj && \
-  git checkout ec9151e8c6909f7fac72bb2eab927ff18fa4cf1d && \
-  python setup.py build && \
-  python setup.py install
-cd ..
-
-# shellcheck disable=2015
-git clone --recursive -b fastmapmatch https://github.com/HSLdevcom/gtfs_shape_mapfit.git && \
-  cd gtfs_shape_mapfit && \
-  make -C pymapmatch
-cd ..
-
-# shellcheck disable=2015
-git clone https://github.com/HSLdevcom/OTPQA.git && \
-  cd OTPQA && \
-  git checkout v3
-cd ..
+git clone https://github.com/HSLdevcom/OTPQA.git && cd OTPQA && git checkout v3
