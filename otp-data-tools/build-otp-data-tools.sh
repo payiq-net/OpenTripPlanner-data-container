3423f1f49dc74bb45f3eb80db62a82893aeea94d
Vesa Meskanen <vesa.meskanen@cgi.com>
2024-02-16 08:38:10 +0200

    Include OBA transformer into this repo

--- otp-data-tools/build-otp-data-tools.sh
+++ otp-data-tools/build-otp-data-tools.sh
@@ -2,14 +2,6 @@
 set -e
 
 apt-get update && apt-get -y install git python3 python3-pip
-
 rm -rf /var/lib/apt/lists/*
-
 pip install future grequests numpy
-
-ONEBUSAWAY_URL="http://nexus.onebusaway.org/service/local/artifact/maven/content?r=public&g=org.onebusaway&a=onebusaway-gtfs-transformer-cli&v=1.3.9"
-
-mkdir -p one-busaway-gtfs-transformer && \
-  wget -O one-busaway-gtfs-transformer/onebusaway-gtfs-transformer-cli.jar $ONEBUSAWAY_URL
-
 git clone -b v3 --single-branch https://github.com/HSLdevcom/OTPQA.git
