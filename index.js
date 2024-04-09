c008dc161b6955cdba56a62b6c7799b2cb12ae36
Vesa Meskanen <vesa.meskanen@cgi.com>
2024-04-09 11:44:55 +0300

    chore: remove unnecessary function wrapper

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const { postSlackMessage } = require('./util')
 
-startBuild()
+postSlackMessage('Starting data build').then(response => {
+  if (response.ok) {
+    global.messageTimeStamp = response.ts
+  }
+  const { update } = require('./task/Update')
+  update()
+}).catch((err) => {
+  console.log(err)
+  const { update } = require('./task/Update')
+  update()
+});
 
-async function startBuild () {
-  postSlackMessage('Starting data build').then(response => {
-    if (response.ok) {
-      global.messageTimeStamp = response.ts
-    }
-    const { update } = require('./task/Update')
-    update()
-  }).catch((err) => {
-    console.log(err)
-    const { update } = require('./task/Update')
-    update()
-  })
-}
