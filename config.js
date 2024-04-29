103b7fbc1d97ad5ec5a6747f99b9b33e1e88131c
sharhio <sade.harhio@cgi.com>
2024-04-29 09:57:31 +0300

    DT-6305 new data for kela

--- config.js
+++ config.js
@@ -117,7 +117,8 @@ const routers = {
     id: 'kela',
     src: [
       mapSrc('kela', 'https://koontikartta.navici.com/tiedostot/gtfs_kela.zip'),
-      mapSrc('matkahuolto', mhAddress, false, ['kela/gtfs-rules/no-onnibus-mega.rule'], { 'transfers.txt': null })
+      mapSrc('matkahuolto', mhAddress, false, ['kela/gtfs-rules/no-onnibus-mega.rule'], { 'transfers.txt': null }),
+      mapSrc('lansilinjat', 'https://lansilinjat.fi/wp-content/uploads/GTFS-Lansilinjat.zip') 
     ],
     osm: ['finland']
   }
