40fd6eee28c23b38dadac9229c2bceacb5fbdf84
sharhio <sade.harhio@cgi.com>
2024-07-29 11:08:20 +0300

    install python packages in virtual env

--- otp-data-tools/build-otp-data-tools.sh
+++ otp-data-tools/build-otp-data-tools.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-apt-get update && apt-get -y install git python3 python3-pip
+apt-get update && apt-get -y install git python3 python3-pip python3-venv
 rm -rf /var/lib/apt/lists/*
-pip install future grequests numpy
+python3 -m venv python-venv && python-venv/bin/pip install future grequests numpy
 git clone -b v3 --single-branch https://github.com/HSLdevcom/OTPQA.git
